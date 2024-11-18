7356433a4f77424829b7b5c66d006e9901addc04
Spookfu <spookfu@proton.me>
2024-11-17 20:50:38 -0500

    Initial commit

---
+++ src/main/java/spookfu/journeys/Journeys.java
@@ -0,0 +1,16 @@
+package spookfu.journeys;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Journeys implements ModInitializer {
+	public static final String MOD_ID = "journeys";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/spookfu/journeys/JourneysClient.java
@@ -0,0 +1,10 @@
+package spookfu.journeys;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class JourneysClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
